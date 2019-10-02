b7f9a017254a4e5633bf2089ad5808d86ce4effe
Guillaume Klaus <guillaumek@theodo.fr>
2019-10-02 13:36:36 +0200

    Fix linter issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,14 +12,14 @@ module.exports = {
     "prettier",
     "eslint:recommended",
     "plugin:vue/recommended",
-    'plugin:prettier/recommended',
-    'prettier/vue'
+    "plugin:prettier/recommended",
+    "prettier/vue"
   ],
   plugins: ["prettier"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
     "prettier/prettier": "error",
-    "no-unused-vars": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-unused-vars": process.env.NODE_ENV === "production" ? "error" : "off"
   }
 };
