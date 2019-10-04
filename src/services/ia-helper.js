e2d07a53d8d9ffd69a61b9fbb62617b7a3fcda69
Antoine <antoiner@theodo.fr>
2019-10-04 14:08:42 +0200

    step 10

--- src/services/ia-helper.js
+++ src/services/ia-helper.js
@@ -1,15 +1,51 @@
-import { ITERATION_LIMIT, getCell, BOARD_SIZE } from "./board-helper";
+import { getNeighbors, getCellIndexes } from "./board-helper";
 
-export function findTargetCell(boardCells) {
-  let cell = "";
+function getCommonDirection(cell1, cell2) {
+  const { row: row1 } = getCellIndexes(cell1);
+  const { row: row2 } = getCellIndexes(cell2);
+  if (row1 === row2) {
+    return ["left", "right"];
+  }
+  return ["up", "down"];
+}
 
-  for (let i = 0; i < ITERATION_LIMIT; i++) {
-    const row = Math.floor(Math.random() * BOARD_SIZE) + 1;
-    const column = Math.floor(Math.random() * BOARD_SIZE) + 1;
-    cell = getCell(row, column);
+function findUnexploredNeighbors(cell, directions, boardCells) {
+  const { row, column } = getCellIndexes(cell);
+  const neighbors = Object.keys(getNeighbors(row, column))
+    .filter(key => directions.includes(key))
+    .map(key => getNeighbors(row, column)[key]);
+  const unexploredCells = neighbors.filter(cellAddress =>
+    ["empty", "ship"].includes(boardCells[cellAddress].status)
+  );
+  return unexploredCells;
+}
 
-    if (["empty", "ship"].includes(boardCells[cell].status)) {
-      return cell;
+function nextTarget(candidates, boardCells) {
+  let filteredDirections = ["up", "down", "left", "right"];
+  if (candidates.length > 1) {
+    filteredDirections = getCommonDirection(candidates[0], candidates[1]);
+  }
+  for (let cell in candidates) {
+    const unexploredNeighbors = findUnexploredNeighbors(
+      candidates[cell],
+      filteredDirections,
+      boardCells
+    );
+    if (unexploredNeighbors.length !== 0) {
+      return unexploredNeighbors[0];
     }
   }
+}
+
+export function findTargetCell(boardCells) {
+  const candidates = Object.keys(boardCells).filter(
+    keyCell => boardCells[keyCell].status === "hit"
+  );
+  if (candidates.length === 0) {
+    const missed = Object.keys(boardCells).filter(
+      keyCell => !["hit", "sunk", "missed"].includes(boardCells[keyCell].status)
+    );
+    return missed[Math.floor(Math.random() * missed.length)];
+  }
+  return nextTarget(candidates, boardCells);
 }
