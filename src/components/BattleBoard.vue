a23df1edbea26b06ccfd3642667813576f9ff246
guiyom-e <guillaume.egee@outlook.com>
2022-07-04 16:08:47 +0200

    step 3

--- src/components/BattleBoard.vue
+++ src/components/BattleBoard.vue
@@ -2,13 +2,19 @@
   <div>
     <div class="battle-board">
       <div class="title">BATTLESHIP</div>
+      <PlayBoard title="Player" :rows-count="10" :columns-count="10" />
     </div>
   </div>
 </template>
 
 <script>
+import PlayBoard from "./PlayBoard.vue";
+
 export default {
   name: "BattleBoard",
+  components: {
+    PlayBoard,
+  },
 };
 </script>
 
---
+++ src/components/PlayBoard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="playboard">
+    <div class="title">{{ title }} {{ columnsCount }} x {{ rowsCount }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PlayBoard",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    columnsCount: {
+      type: Number,
+      default: 10,
+    },
+    rowsCount: {
+      type: Number,
+      default: 10,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.title {
+  font-size: 18px;
+  font-weight: 500;
+  margin-bottom: 10px;
+  text-align: center;
+}
+</style>
