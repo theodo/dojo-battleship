1dff6488aeae956907fcb87543b6620c151a952d
guiyom-e <guillaume.egee@outlook.com>
2022-07-04 16:12:28 +0200

    step 4

--- src/components/PlayBoard.vue
+++ src/components/PlayBoard.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="playboard">
-    <div class="title">{{ title }} {{ columnsCount }} x {{ rowsCount }}</div>
+    <div class="title">{{ title }}</div>
+    <div class="grid-container">
+      <div v-for="row in rowsCount" :key="'row'.concat(row)" class="line">
+        <div
+          v-for="column in columnsCount"
+          :key="''.concat(column).concat('-'.concat(row))"
+          class="cell"
+        ></div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -30,5 +39,30 @@ export default {
   font-weight: 500;
   margin-bottom: 10px;
   text-align: center;
+}
+
+.playboard {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.line {
+  margin: 0px;
+  width: fit-content;
+  display: flex;
+  border-top: 1px solid black;
+  border-bottom: 1px solid black;
+  & + & {
+    border-top: none;
+  }
+}
+.cell {
+  border-left: 1px solid black;
+  border-right: 1px solid black;
+  & + & {
+    border-left: none;
+  }
+  height: 3vw;
+  width: 3vw;
 }
 </style>
